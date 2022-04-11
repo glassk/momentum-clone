218561ac4fb0eb5d8546d05c39b966eb8211e48a
YuriKim <glassk0724@gmail.com>
2022-04-12 05:00:49 +0900

    Arrays

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-// const amIFat = true;
-const amIFat = null;
-let something;
+const dayOfWeek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
-console.log(something, amIFat); // undefined null
+console.log(dayOfWeek[4]); // fri
+
+// Get Item from Array
+console.log(dayOfWeek); // (6) ['mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+
+// Add one more day to the array
+dayOfWeek.push('sun');
+
+console.log(dayOfWeek); // (7) ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
