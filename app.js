aa520147aedac745487f51513b55a19ce3dd20ad
YuriKim <glassk0724@gmail.com>
2022-04-13 16:09:45 +0900

    Functions

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
+// Example #1
+function sayHello() {
+  console.log('Hello!');
+}
+
+sayHello(); // Hello!
+sayHello(); // Hello!
+
+alert();
+console.log(); // 아무것도 출력하지 않는다
+
+// Example #2
+function sayHello(nameOfPerson, age) {
+  console.log('Hello! my name is ' + nameOfPerson + " and I'm " + age);
+}
+
+sayHello('nico', 10);
+sayHello('dal', 23);
+sayHello('lynn', 21);
+
+// Example #3
+function plus(firstNumber, secondNumber) {
+  console.log(firstNumber + secondNumber);
+}
+console.log(firstNumber); // Uncaught ReferenceError: firstNumber is not defined
+
+function divide(a, b) {
+  console.log(a / b);
+}
+
+plus(8, 60); // 68
+divide(98, 20); // 4.9
+
+// Example #4
 const player = {
   name: 'nico',
-  points: 10,
-  fat: true,
+  sayHello: function (otherPersonsName) {
+    console.log('hello! ' + otherPersonsName + ' nice to meet you!');
+  },
 };
 
-console.log(player); // {name: 'nico', points: 10, fat: true}
-console.log(player.name); // nico
-console.log(player['name']); // nico
-
-player.fat = false;
-console.log(player); // {name: 'nico', points: 10, fat: false}
-
-player.lastName = 'potato';
-console.log(player); // {name: 'nico', points: 10, fat: false, lastName: 'potato'}
-
-player.points = player.points + 15;
-console.log(player); //{name: 'nico', points: 25, fat: false, lastName: 'potato'}
+console.log(player.name);
+player.sayHello('lynn');
+player.sayHello('nico');
