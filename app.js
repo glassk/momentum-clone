bef61ab317d38eadc91fce2c5948b1373e70f271
YuriKim <glassk0724@gmail.com>
2022-04-13 16:23:00 +0900

    Returns

--- app.js
+++ app.js
@@ -1,44 +1,35 @@
 // Example #1
-function sayHello() {
-  console.log('Hello!');
-}
-
-sayHello(); // Hello!
-sayHello(); // Hello!
-
-alert();
-console.log(); // 아무것도 출력하지 않는다
-
-// Example #2
-function sayHello(nameOfPerson, age) {
-  console.log('Hello! my name is ' + nameOfPerson + " and I'm " + age);
-}
-
-sayHello('nico', 10);
-sayHello('dal', 23);
-sayHello('lynn', 21);
-
-// Example #3
-function plus(firstNumber, secondNumber) {
-  console.log(firstNumber + secondNumber);
-}
-console.log(firstNumber); // Uncaught ReferenceError: firstNumber is not defined
-
-function divide(a, b) {
-  console.log(a / b);
-}
-
-plus(8, 60); // 68
-divide(98, 20); // 4.9
-
-// Example #4
-const player = {
-  name: 'nico',
-  sayHello: function (otherPersonsName) {
-    console.log('hello! ' + otherPersonsName + ' nice to meet you!');
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  times: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  power: function (a, b) {
+    return a ** b;
   },
 };
 
-console.log(player.name);
-player.sayHello('lynn');
-player.sayHello('nico');
+// 상호의존적(서로 연결됨)
+const plusResult = calculator.plus(2, 3); // 5
+const minusResult = calculator.minus(plusResult, 10); // -5
+const timesResult = calculator.times(10, minusResult); // -50
+const divideResult = calculator.divide(timesResult, plusResult); // -45
+const powerResult = calculator.power(divideResult, minusResult); //
+
+// Example #2
+const age = 96;
+function calculateKrAge(ageOfForeigner) {
+  return ageOfForeigner + 2;
+}
+
+const krAge = calculateKrAge(age);
+
+// console.log(krAge); // 98
