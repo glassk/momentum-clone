9617ba8abfbf07afad8a0536d3b2a9a8fa15ede2
YuriKim <glassk0724@gmail.com>
2022-04-14 12:23:30 +0900

    Events

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const title = document.querySelector('.hello h1');
 
-title.innerText = 'Hello';
-console.log(title);
+console.dir(title);
+
+function handleTitleClick() {
+  title.style.color = 'blue';
+  console.log('title was clicked!');
+}
+
+function handleMouseEnter() {
+  title.innerText = 'Mouse is here!';
+}
+
+function handleMouseLeave() {
+  title.innerText = 'Mouse is gone!';
+}
+
+title.addEventListener('click', handleTitleClick);
+title.addEventListener('mouseenter', handleMouseEnter);
+title.addEventListener('mouseleave', handleMouseLeave);
