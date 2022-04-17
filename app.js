7b864ddd180f85ff2d37d51c952985d3110079b4
YuriKim <glassk0724@gmail.com>
2022-04-17 10:59:48 +0900

    Input Values

--- app.js
+++ app.js
@@ -1,35 +1,8 @@
-const h1 = document.querySelector('div.hello:first-child h1');
+const loginInput = document.querySelector('#login-form input');
+const loginButton = document.querySelector('#login-form button');
 
-function handleTitleClick() {
-  // Example #1
-  const currentColor = h1.style.color;
-  let newColor;
-
-  if (currentColor === 'blue') {
-    newColor = 'tomato';
-  } else {
-    newColor = 'blue';
-  }
-
-  h1.style.color = newColor;
-
-  // Example #2
-  const clickedClass = 'clicked';
-  if (h1.className === clickedClass) {
-    h1.className = '';
-  } else {
-    h1.className = clickedClass;
-  }
-
-  // Example #3
-  if (h1.classList.contains(clickedClass)) {
-    h1.classList.remove(clickedClass);
-  } else {
-    h1.classList.add(clickedClass);
-  }
-
-  // Example #4
-  h1.classList.toggle('clicked');
+function onLoginButtonClick() {
+  console.log('hello', loginInput.value);
 }
 
-h1.addEventListener('click', handleTitleClick);
+loginButton.addEventListener('click', onLoginButtonClick);
