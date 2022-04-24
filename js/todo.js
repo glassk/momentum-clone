73dcae15425bef8c4c756e933b7b55d019420fed
YuriKim <glassk0724@gmail.com>
2022-04-24 17:56:47 +0900

    Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -13,12 +13,16 @@ function saveToDos() {
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintToDo(newToDo) {
   const li = document.createElement('li');
+  li.id = newToDo.id;
+
   const span = document.createElement('span');
-  span.innerText = newToDo;
+  span.innerText = newToDo.text;
 
   const button = document.createElement('button');
   button.innerText = '❌';
@@ -34,8 +38,13 @@ function handleToDoSubmit(event) {
 
   const newToDo = toDoInput.value;
   toDoInput.value = '';
-  toDos.push(newToDo);
-  paintToDo(newToDo);
+  const newToDoObj = {
+    text: newToDo,
+    id: Date.now(),
+  };
+
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
   saveToDos();
 }
 
@@ -46,5 +55,5 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
-  parsedToDos.forEach((item) => paintToDo(item));
+  parsedToDos.forEach(paintToDo);
 }
